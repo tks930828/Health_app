e94117b42eed083fa1bfd6188985d54babda096f
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-05-17 18:28:08 +0900

    st-submit_button

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import streamlit as st
 
-
+#食事入力フォーム
 with st.form('eat_form'):
     date = st.datetime_input("日付を入力してください")
     meal = st.text_area("食事名を入力してください")
     
+
+    submitted = st.form_submit_button("送信")
 
 
