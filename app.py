293dcec9801c72aae4409c34f582f19f0f01f519
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-05-18 07:59:07 +0900

    meal-form

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ import streamlit as st
 with st.form('eat_form'):
     date = st.datetime_input("日付を入力してください")
     meal = st.text_area("食事名を入力してください")
-    
+    calories = st.text_area("カロリーを入力してください")
+    protein = st.text_area("タンパク質を入力してください")
+    fat = st.text_area("脂質を入力してください")
+    carb = st.text_area("炭水化物を入力してください")
+
 
     submitted = st.form_submit_button("送信")
 
