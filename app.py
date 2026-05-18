c59af28a9c5c75db70c23f528e04f99389cc85eb
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-05-19 07:43:36 +0900

    weight-form

--- app.py
+++ app.py
@@ -2,14 +2,30 @@ import streamlit as st
 
 #食事入力フォーム
 with st.form('eat_form'):
-    date = st.datetime_input("日付を入力してください")
-    meal = st.text_area("食事名を入力してください")
-    calories = st.text_area("カロリーを入力してください")
-    protein = st.text_area("タンパク質を入力してください")
-    fat = st.text_area("脂質を入力してください")
-    carb = st.text_area("炭水化物を入力してください")
+    meal_date = st.date_input("日付を入力してください")
+    meal = st.text_input("食事名を入力してください")
+    calories = st.number_input(
+    "カロリー(kcal)",
+    min_value=0,
+    step=1
+)
+    protein = st.number_input("タンパク質を入力してください")
+    fat = st.number_input("脂質を入力してください")
+    carb = st.number_input("炭水化物を入力してください")
 
+    meal_submitted = st.form_submit_button("送信")
+    if meal_submitted:
+        st.success("食事を登録しました")
 
-    submitted = st.form_submit_button("送信")
+#体重入力フォーム
+with st.form('weight_form'):
+    weight_date = st.datetime_input("日付を入力してください")
+    weight = st.number_input(
+    "体重(kg)",
+    min_value=0.0,
+    step=0.1
+)
 
-
+    weight_submitted = st.form_submit_button("送信")
+    if weight_submitted:
+        st.success("体重を登録しました")
