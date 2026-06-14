8a61565be7839f0b953e567e212fc44487991116
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-06-14 19:20:42 +0900

    st.expander-meal-up/del

--- pages/meal_page.py
+++ pages/meal_page.py
@@ -82,130 +82,135 @@ def meal_page(session):
 
     #pandas_dataframe（2次元データ）
     df_meals = pd.DataFrame(meal_data)
-
     #streamlit表示
     st.dataframe(df_meals)
 
-    # 食事データからID一覧を取得
-    meal_ids = [meal.id for meal in meals]
+    #食事記録編集の折り畳み
+    with st.expander("食事記録編集"):
+        #食事記録の削除
+        st.subheader("編集")
 
-    if meal_ids:
+        # 食事データからID一覧を取得
+        meal_ids = [meal.id for meal in meals]
 
-        #編集するIDを選択
-        selected_id = st.selectbox(
-            "編集する食事",
-            meal_ids,
-            key = "meal_update_id"
-        )
+        if meal_ids:
 
-        #DBから対象データを取得
-        meal = session.query(Meal).filter(
-                    Meal.id == selected_id
-                ).first()
-        
-        if meal:
-
-            new_date = st.date_input(
-                "日付",
-                value=meal.date
+            #編集するIDを選択
+            selected_id = st.selectbox(
+                "編集する食事",
+                meal_ids,
+                key = "meal_update_id"
             )
 
-            new_meal_type = st.selectbox(
-                "食事区分",
-                ["朝食", "昼食", "夕食", "間食"],
-                index=["朝食", "昼食", "夕食", "間食"].index(meal.meal_type)
-            )
-
-            new_meal_name = st.text_input(
-                "食事名",
-                value=meal.meal_name
-            )
-
-            new_calories = st.number_input(
-                "カロリー",
-                value=int(meal.calories)
-            )
-            
-            new_protein = st.number_input(
-                "プロテイン(g)",
-                value=float(meal.protein)
-            )
-
-            new_fat = st.number_input(
-                "脂肪(g)",
-                value=float(meal.fat)
-            )
-
-            new_carb = st.number_input(
-                "炭水化物",
-                value=float(meal.carb)
-            )
-
-        else:
-            st.error("対象データが見つかりません")
-
-        #更新ボタン
-        if st.button(
-            "更新",
-            key = "meal_update_button"
-        ):
-        
-            #値を更新
-            meal.date = new_date
-            meal.meal_type = new_meal_type
-            meal.meal_name = new_meal_name
-            meal.calories = new_calories
-            meal.protein = new_protein
-            meal.fat = new_fat
-            meal.carb = new_carb
-
-            #commit
-            session.commit()
-            st.success("更新しました")
-            st.rerun()
-
-    else:
-        st.info("編集できるデータがありません")
-
-    #食事記録の削除
-    st.subheader("削除")
-
-    # 食事データからID一覧を取得
-    meal_ids = [meal.id for meal in meals]
-
-    if meal_ids:
-
-     #削除するIDを選択
-        selected_id = st.selectbox(
-            "削除するID",
-            meal_ids,
-            key = "meal_delete_id"
-        )
-
-        #削除ボタンが押された場合に削除を実行する
-        if st.button(
-            "削除",
-            key="meal_delete_button"
-        ):
-        
+            #DBから対象データを取得
             meal = session.query(Meal).filter(
-                Meal.id == selected_id
-            ).first()
-
+                        Meal.id == selected_id
+                    ).first()
+            
             if meal:
 
-                #レコード削除
-                session.delete(meal)
-                #DBに反映
-                session.commit()
+                new_date = st.date_input(
+                    "日付",
+                    value=meal.date
+                )
 
-                st.success("削除しました")
-                st.rerun()
-        
+                new_meal_type = st.selectbox(
+                    "食事区分",
+                    ["朝食", "昼食", "夕食", "間食"],
+                    index=["朝食", "昼食", "夕食", "間食"].index(meal.meal_type)
+                )
+
+                new_meal_name = st.text_input(
+                    "食事名",
+                    value=meal.meal_name
+                )
+
+                new_calories = st.number_input(
+                    "カロリー",
+                    value=int(meal.calories)
+                )
+                
+                new_protein = st.number_input(
+                    "プロテイン(g)",
+                    value=float(meal.protein)
+                )
+
+                new_fat = st.number_input(
+                    "脂肪(g)",
+                    value=float(meal.fat)
+                )
+
+                new_carb = st.number_input(
+                    "炭水化物",
+                    value=float(meal.carb)
+                )
+
             else:
-                st.error("データが存在しません")
+                st.error("対象データが見つかりません")
+
+            #更新ボタン
+            if st.button(
+                "更新",
+                key = "meal_update_button"
+            ):
+            
+                #値を更新
+                meal.date = new_date
+                meal.meal_type = new_meal_type
+                meal.meal_name = new_meal_name
+                meal.calories = new_calories
+                meal.protein = new_protein
+                meal.fat = new_fat
+                meal.carb = new_carb
+
+                #commit
+                session.commit()
+                st.success("更新しました")
+                st.rerun()
+        else:
+            st.info("編集できるデータがありません")
         
-    else:
-         st.info("削除できるデータがありません")
-    
-    return df_meals
+
+    with st.expander("食事記録削除"):
+        #食事記録の削除
+        st.subheader("削除")
+
+        # 食事データからID一覧を取得
+        meal_ids = [meal.id for meal in meals]
+
+        if meal_ids:
+
+        #削除するIDを選択
+            selected_id = st.selectbox(
+                "削除するID",
+                meal_ids,
+                key = "meal_delete_id"
+            )
+
+            #削除ボタンが押された場合に削除を実行する
+            if st.button(
+                "削除",
+                key="meal_delete_button"
+            ):
+            
+                meal = session.query(Meal).filter(
+                    Meal.id == selected_id
+                ).first()
+
+                if meal:
+
+                    #レコード削除
+                    session.delete(meal)
+                    #DBに反映
+                    session.commit()
+
+                    st.success("削除しました")
+                    st.rerun()
+            
+                else:
+                    st.error("データが存在しません")
+            
+        else:
+            st.info("削除できるデータがありません")
+        
+        return df_meals
