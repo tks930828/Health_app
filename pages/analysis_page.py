cd9dd99670b3a6e11bad15265d29114d975019a2
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-07-02 23:16:27 +0900

    refa-ana0702

--- pages/analysis_page.py
+++ pages/analysis_page.py
@@ -43,75 +43,7 @@ def create_pfc_chart(
                 Carb : {total_carb:.1f} g
                     """
                 )
-
-def create_meal_type_chart(
-            df_meals,
-            ):
-
-            # 食事区分別カロリー推移
-            meal_type_calories = (
-            df_meals.groupby("meal_type")["calories"]
-            .sum()
-            .reset_index()
-            )
-
-            fig  = px.bar(
-                meal_type_calories,
-                x="meal_type",
-                y="calories",
-                title="食事区分別カロリー"
-            )
-
-            st.plotly_chart(
-                fig,
-                width="stretch"
-            )
-
-
-
-def analysis_page(
-        df_meals,
-        df_weights,
-):
-    
-    st.subheader("健康サマリー")
-
-    col1, col2, col3, col4 = st.columns(4)
-
-    with col1:
-        st.metric(
-            "総摂取カロリー",
-            f"{total_calories:.0f} kcal"
-        )
-    with col2:
-        st.metric(
-            "平均体重",
-            f"{average_weight:.1f} kg"
-        )
-    with col3:
-        st.metric(
-            "記録日数",
-            f"{record_days} 日"
-        )
-    with col4:
-        st.metric(
-            "体重変化",
-            f"{end_weight:.1f} kg"
-            if end_weight is not None else "-",
-            delta=f"{weight_change:+.1f} kg"
-            if weight_change is not None else "-"
-        )
-        
-    st.header("分析")
-    tab1, tab2, tab3 = st.tabs(
-        ["サマリー", "食事分析", "体重分析"]
-    )
-
-    with tab1:
-
-        col1, col2 = st.columns(2)
-
-        with col1:
+            
             #円グラフの作成
             if not df_meals.empty:
 
@@ -144,41 +76,52 @@ def analysis_page(
 
             else:
                 st.info("食事データがありません")
-        
-        with col2:
-            
-            
-   
-    with tab2:
 
-        col1, col2 = st.columns(2)
-
-        with col1:
-        
-        with col2:
-
-            def create_monthly_chart(
+def create_meal_type_chart(
             df_meals,
             ):
+
+            # 食事区分別カロリー推移
+            meal_type_calories = (
+            df_meals.groupby("meal_type")["calories"]
+            .sum()
+            .reset_index()
+            )
+
+            fig  = px.bar(
+                meal_type_calories,
+                x="meal_type",
+                y="calories",
+                title="食事区分別カロリー"
+            )
+
+            st.plotly_chart(
+                fig,
+                width="stretch"
+            )
+
+def create_monthly_chart(
+    df_meals,
+    ):
             
-                #月別カロリー推移
-                if not df_meals.empty:
+        #月別カロリー推移
+        if not df_meals.empty:
 
-                    df_month = df_meals.copy()
+            df_month = df_meals.copy()
 
-                    df_month["month"] = (
+            df_month["month"] = (
                     pd.to_datetime(df_month["date"])
                     .dt.strftime("%Y-%m")
-                    )
+                )
 
-                    monthly_calories = (
+                monthly_calories = (
                     df_month
                     .groupby("month")["calories"]
                     .sum()
                     .reset_index()
-                    )
+                )
                 
-                    fig = px.line(
+                fig = px.line(
                         monthly_calories,
                         x="month",
                         y="calories",
@@ -186,161 +129,152 @@ def analysis_page(
                         markers=True
                     )
 
-                    st.plotly_chart(
-                        fig,
-                        width="stretch"
-                    )
+                st.plotly_chart(
+                    fig,
+                    width="stretch"
+                )
 
-            def create_daily_chart(
+def create_daily_chart(
+    df_meals,
+    ):
+
+    #食事推移グラフの作成
+    if not df_meals.empty:
+
+        daily_calories = (
+            df_meals.groupby("date")["calories"]
+            .sum()
+            .reset_index()
+        )
+
+        fig = px.bar(
+            daily_calories,
+            x="date",
+            y="calories",
+            title="日別カロリー摂取量"
+        )
+
+        fig.update_xaxes(type="category")
+
+        #グラフの表示
+        st.plotly_chart(fig)
+
+        else:
+            st.info("食事データがありません")
+
+def create_weight_chart(
+    df_weights,
+    ):
+
+    #体重推移グラフ（折れ線グラフ）の作成
+    if not df_weights.empty:
+            
+        fig = px.line(
+            df_weights,
+            x = 'date',
+            y = 'weight',
+            title = '体重推移',
+            markers = True
+        )
+
+            #x軸をカテゴリ軸として扱う
+            fig.update_xaxes(type='category')
+
+            #グラフの表示
+            st.plotly_chart(fig)
+        else:
+            st.info("体重データがありません")
+
+def create_scatter_chart(
+    df_meals,
+    df_weights,
+    ):
+
+    daily_calories = (
+        df_meals.groupby("date")["calories"]
+        .sum()
+        .reset_index()
+        )
+
+    #カロリーと体重の関係分析
+    #体重とカロリーを日付で結合
+    merged_df = pd.merge(
+    daily_calories,
+    df_weights,
+    on="date",
+    how="inner"
+    )
+
+    #散布図
+    fig = px.scatter(
+        merged_df,
+        x="calories",
+        y="weight",
+        title="摂取カロリーと体重"
+    )
+
+    st.plotly_chart(
+        fig,
+        width="stretch"
+    )
+
+#関数
+def analysis_page(
+        df_meals,
+        df_weights,
+):
+
+    tab1, tab2, tab3 = st.tabs(
+        ["サマリー", "食事分析", "体重分析"]
+    )
+
+    with tab1:
+        create_kpi(
             df_meals,
-            ):
+            df_weights
+        )
 
-                #食事推移グラフの作成
-                if not df_meals.empty:
+        col1, col2 = st.columns(2)
 
-                    daily_calories = (
-                    df_meals.groupby("date")["calories"]
-                    .sum()
-                    .reset_index()
-                    )
+        with col1:
+            create_pfc_chart(
+                df_meals
+            )
 
-                    fig = px.bar(
-                        daily_calories,
-                        x="date",
-                        y="calories",
-                        title="日別カロリー摂取量"
-                    )
+        with col2:
+            create_pfc_summary(
+                df_meals
+            )
 
-                    fig.update_xaxes(type="category")
+    with tab2:
 
-                    #グラフの表示
-                    st.plotly_chart(fig)
+        col1, col2 = st.columns(2)
 
-                else:
-                    st.info("食事データがありません")
+        with col1:
+            create_meal_type_chart(
+                df_meals
+            )
+
+        with col2:
+            create_monthly_chart(
+                df_meals
+            )
+
+        create_daily_chart(
+            df_meals
+        )
 
     with tab3:
 
         col1, col2 = st.columns(2)
 
         with col1:
+            create_weight_chart(
+                df_weights
+            )
 
-            def create_weight_chart(
-            df_weights,
-            ):
-
-                #体重推移グラフ（折れ線グラフ）の作成
-                if not df_weights.empty:
-        
-                    fig = px.line(
-                        df_weights,
-                        x = 'date',
-                        y = 'weight',
-                        title = '体重推移',
-                        markers = True
-                    )
-
-                    #x軸をカテゴリ軸として扱う
-                    fig.update_xaxes(type='category')
-
-                    #グラフの表示
-                    st.plotly_chart(fig)
-                else:
-                    st.info("体重データがありません")
-    
         with col2:
-
-            def create_scatter_chart(
-            df_meals,
-            df_weights,
-            ):
-
-                daily_calories = (
-                    df_meals.groupby("date")["calories"]
-                    .sum()
-                    .reset_index()
-                    )
-
-                #カロリーと体重の関係分析
-                #体重とカロリーを日付で結合
-                merged_df = pd.merge(
-                daily_calories,
-                df_weights,
-                on="date",
-                how="inner"
-                )
-
-                #散布図
-                fig = px.scatter(
-                    merged_df,
-                    x="calories",
-                    y="weight",
-                    title="摂取カロリーと体重"
-                )
-
-                st.plotly_chart(
-                    fig,
-                    width="stretch"
-                )
-
-#関数分割
-# def analysis_page(
-#         df_meals,
-#         df_weights,
-# ):
-
-#     tab1, tab2, tab3 = st.tabs(
-#         ["サマリー", "食事分析", "体重分析"]
-#     )
-
-#     with tab1:
-#         create_kpi(
-#             df_meals,
-#             df_weights
-#         )
-
-#         col1, col2 = st.columns(2)
-
-#         with col1:
-#             create_pfc_chart(
-#                 df_meals
-#             )
-
-#         with col2:
-#             create_pfc_summary(
-#                 df_meals
-#             )
-
-#     with tab2:
-
-#         col1, col2 = st.columns(2)
-
-#         with col1:
-#             create_meal_type_chart(
-#                 df_meals
-#             )
-
-#         with col2:
-#             create_monthly_chart(
-#                 df_meals
-#             )
-
-#         create_daily_chart(
-#             df_meals
-#         )
-
-#     with tab3:
-
-#         col1, col2 = st.columns(2)
-
-#         with col1:
-#             create_weight_chart(
-#                 df_weights
-#             )
-
-#         with col2:
-#             create_scatter_chart(
-#                 df_meals,
-#                 df_weights
-#             )
-# """"""
+            create_scatter_chart(
+                df_meals,
+                df_weights
+            )
