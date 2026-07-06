e8e48adffe75df5b958cf1d034e53b1fdba5f3b8
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-07-06 21:33:45 +0900

    mk-meal_chart

---
+++ charts/meal_chart.py
@@ -0,0 +1,88 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+
+def create_meal_type_chart(
+        df_meals,
+    ):
+
+    # 食事区分別カロリー推移
+    meal_type_calories = (
+        df_meals.groupby("meal_type")["calories"]
+        .sum()
+        .reset_index()
+    )
+
+    fig  = px.bar(
+        meal_type_calories,
+        x="meal_type",
+        y="calories",
+        title="食事区分別カロリー"
+    )
+
+    st.plotly_chart(
+        fig,
+        width="stretch"
+    )
+
+def create_monthly_chart(
+        df_meals,
+    ):
+            
+    #月別カロリー推移
+    if not df_meals.empty:
+
+        df_month = df_meals.copy()
+
+        df_month["month"] = (
+                pd.to_datetime(df_month["date"])
+                .dt.strftime("%Y-%m")
+            )
+
+    monthly_calories = (
+        df_month
+        .groupby("month")["calories"]
+        .sum()
+        .reset_index()
+    )
+                
+    fig = px.line(
+            monthly_calories,
+            x="month",
+            y="calories",
+            title="月別カロリー推移",
+            markers=True
+        )
+
+    st.plotly_chart(
+        fig,
+        width="stretch"
+    )
+
+def create_daily_chart(
+    df_meals,
+    ):
+
+    #食事推移グラフの作成
+    if not df_meals.empty:
+
+        daily_calories = (
+            df_meals.groupby("date")["calories"]
+            .sum()
+            .reset_index()
+        )
+
+        fig = px.bar(
+            daily_calories,
+            x="date",
+            y="calories",
+            title="日別カロリー摂取量"
+        )
+
+        fig.update_xaxes(type="category")
+
+        #グラフの表示
+        st.plotly_chart(fig)
+
+    else:
+        st.info("食事データがありません")
--- pages/analysis_page.py
+++ pages/analysis_page.py
@@ -2,9 +2,13 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
+#関数分割
 from charts.kpi_chart import create_kpi
 from charts.pfc_chart import create_pfc_summary
 from charts.pfc_chart import create_pfc_chart
+from charts.meal_chart import create_meal_type_chart
+from charts.meal_chart import create_monthly_chart
+from charts.meal_chart import create_daily_chart
 
 def analysis_page(
         df_meals,
@@ -24,90 +28,17 @@ def analysis_page(
         df_meals,
     )
 
-def create_meal_type_chart(
+    create_meal_type_chart(
         df_meals,
-    ):
-
-    # 食事区分別カロリー推移
-    meal_type_calories = (
-        df_meals.groupby("meal_type")["calories"]
-        .sum()
-        .reset_index()
     )
 
-    fig  = px.bar(
-        meal_type_calories,
-        x="meal_type",
-        y="calories",
-        title="食事区分別カロリー"
-    )
-
-    st.plotly_chart(
-        fig,
-        width="stretch"
-    )
-
-def create_monthly_chart(
+    create_monthly_chart(
         df_meals,
-    ):
-            
-    #月別カロリー推移
-    if not df_meals.empty:
-
-        df_month = df_meals.copy()
-
-        df_month["month"] = (
-                pd.to_datetime(df_month["date"])
-                .dt.strftime("%Y-%m")
-            )
-
-    monthly_calories = (
-        df_month
-        .groupby("month")["calories"]
-        .sum()
-        .reset_index()
-    )
-                
-    fig = px.line(
-            monthly_calories,
-            x="month",
-            y="calories",
-            title="月別カロリー推移",
-            markers=True
-        )
-
-    st.plotly_chart(
-        fig,
-        width="stretch"
     )
 
-def create_daily_chart(
+    create_daily_chart(
     df_meals,
-    ):
-
-    #食事推移グラフの作成
-    if not df_meals.empty:
-
-        daily_calories = (
-            df_meals.groupby("date")["calories"]
-            .sum()
-            .reset_index()
-        )
-
-        fig = px.bar(
-            daily_calories,
-            x="date",
-            y="calories",
-            title="日別カロリー摂取量"
-        )
-
-        fig.update_xaxes(type="category")
-
-        #グラフの表示
-        st.plotly_chart(fig)
-
-    else:
-        st.info("食事データがありません")
+    )
 
 def create_weight_chart(
     df_weights,
