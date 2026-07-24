8323d5007dea85d807855aedb2900516992c94bc
tks930828 <196395209+tks930828@users.noreply.github.com>
2026-07-24 11:43:50 +0900

    add-calorie-activity

--- charts/calorie_chart.py
+++ charts/calorie_chart.py
@@ -7,10 +7,17 @@ def create_calorie_info(
     session,
     df_weights,
 ):
-    #プロフィール情報を取得
+
     profile = session.query(Profile).first()
 
-    #体重取得
+    if profile is None:
+        st.info("プロフィールを登録してください。")
+        return
+
+    if df_weights.empty:
+        st.info("体重を登録してください。")
+        return
+
     weight = df_weights["weight"].iloc[-1]
 
     if profile.sex == "男性":
@@ -30,3 +37,33 @@ def create_calorie_info(
             - 5 * profile.age
             - 161
         )
+
+    activity = {
+        "低い": 1.2,
+        "普通": 1.55,
+        "高い": 1.725,
+    }
+
+    activity_factor = activity.get(
+        profile.activity_level,
+        1.2
+    )
+
+    recommended = (
+        bmr
+        * activity_factor
+    )
+
+    st.metric(
+        "基礎代謝",
+        f"{bmr:.0f} kcal"
+    )
+
+    st.metric(
+        "推奨摂取カロリー",
+        f"{recommended:.0f} kcal"
+    )
+
+    st.caption(
+    "※ 推奨摂取カロリーは活動レベルを考慮して計算しています。"
+    )
